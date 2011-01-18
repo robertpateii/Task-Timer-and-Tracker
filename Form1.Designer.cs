89d1480e7cea0871c441225180191ead9eebb701
robertpateii <robert.pate@gmail.com>
2011-01-18 00:54:01 -0600

    Initial Commit
    Currently builds, runs, and tracks time with Check In/Out options.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TaskTracker
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.loginButton = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.logoutButton = new System.Windows.Forms.Button();
+			this.taskTimeLabel = new System.Windows.Forms.Label();
+			this.infoLinkLabel = new System.Windows.Forms.LinkLabel();
+			this.SuspendLayout();
+			// 
+			// loginButton
+			// 
+			this.loginButton.Location = new System.Drawing.Point(43, 72);
+			this.loginButton.Name = "loginButton";
+			this.loginButton.Size = new System.Drawing.Size(75, 23);
+			this.loginButton.TabIndex = 0;
+			this.loginButton.Text = "Check In";
+			this.loginButton.UseVisualStyleBackColor = true;
+			this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 5000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// logoutButton
+			// 
+			this.logoutButton.Location = new System.Drawing.Point(160, 72);
+			this.logoutButton.Name = "logoutButton";
+			this.logoutButton.Size = new System.Drawing.Size(75, 23);
+			this.logoutButton.TabIndex = 1;
+			this.logoutButton.Text = "Check Out";
+			this.logoutButton.UseVisualStyleBackColor = true;
+			this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+			// 
+			// taskTimeLabel
+			// 
+			this.taskTimeLabel.AutoSize = true;
+			this.taskTimeLabel.BackColor = System.Drawing.SystemColors.Info;
+			this.taskTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.taskTimeLabel.ForeColor = System.Drawing.SystemColors.InfoText;
+			this.taskTimeLabel.Location = new System.Drawing.Point(56, 25);
+			this.taskTimeLabel.Name = "taskTimeLabel";
+			this.taskTimeLabel.Size = new System.Drawing.Size(160, 39);
+			this.taskTimeLabel.TabIndex = 2;
+			this.taskTimeLabel.Text = "0d 0h 0m";
+			this.taskTimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// infoLinkLabel
+			// 
+			this.infoLinkLabel.AutoSize = true;
+			this.infoLinkLabel.Location = new System.Drawing.Point(243, 101);
+			this.infoLinkLabel.Name = "infoLinkLabel";
+			this.infoLinkLabel.Size = new System.Drawing.Size(24, 13);
+			this.infoLinkLabel.TabIndex = 3;
+			this.infoLinkLabel.TabStop = true;
+			this.infoLinkLabel.Text = "info";
+			this.infoLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.infoLinkLabel_LinkClicked);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(271, 117);
+			this.Controls.Add(this.infoLinkLabel);
+			this.Controls.Add(this.taskTimeLabel);
+			this.Controls.Add(this.logoutButton);
+			this.Controls.Add(this.loginButton);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Time Counter";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button loginButton;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button logoutButton;
+		private System.Windows.Forms.Label taskTimeLabel;
+		private System.Windows.Forms.LinkLabel infoLinkLabel;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TaskTracker
+{
+	public partial class Form1 : Form
+	{
+		TaskTimer myTaskTimer;
+		const string saveFileName = "TaskTimeData.txt";
+		const string myWebsiteLink = "http://robertpateii.com";
+		public Form1()
+		{
+			InitializeComponent();
+			if (File.Exists(saveFileName) == true)
+			{
+				string savedTaskTime = File.ReadAllText(saveFileName);
+				if (!string.IsNullOrEmpty(savedTaskTime))
+				{
+					// Converts from the saved string to a TimeSpan object.
+					TimeSpan savedTaskTimeSpan = TimeSpan.Parse(savedTaskTime);
+					myTaskTimer = new TaskTimer(savedTaskTimeSpan);
+				}
+				else
+				{
+					myTaskTimer = new TaskTimer(TimeSpan.Zero);
+				}
+			}
+			else
+			{
+				myTaskTimer = new TaskTimer(TimeSpan.Zero);
+			}
+			// 10 second interval. This is set in "Form1.Designer.cs" to a different value, but the value here takes precedence.
+			timer1.Interval = 10000;
+			logoutButton.Enabled = false;
+			updateMyTaskTime();
+		}
+
+		private void loginButton_Click(object sender, EventArgs e)
+		{
+			myTaskTimer.Start(); // Starts the core timer
+			timer1.Start(); // Starts the form's timer used to update the GUI
+			loginButton.Enabled = false;
+			logoutButton.Enabled = true;
+			updateMyTaskTime();
+			saveMyTaskTime();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			updateMyTaskTime();
+		}
+
+		private void logoutButton_Click(object sender, EventArgs e)
+		{
+			myTaskTimer.Pause();
+			timer1.Stop();
+			loginButton.Enabled = true;
+			logoutButton.Enabled = false;
+			updateMyTaskTime();
+			saveMyTaskTime();
+		}
+
+		private void updateMyTaskTime()
+		{
+			TimeSpan myTaskTime;
+			myTaskTime = myTaskTimer.TaskTime;
+			string taskTimeString = myTaskTime.Days + "d " + myTaskTime.Hours + "h " + myTaskTime.Minutes + "m";
+			taskTimeLabel.Text = taskTimeString;
+		}
+
+		private void saveMyTaskTime()
+		{
+			File.WriteAllText(saveFileName, myTaskTimer.TaskTime.ToString());
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			saveMyTaskTime();
+		}
+
+		private void infoLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			MessageBox.Show("This program saves your data everytime you Check In, Check Out, or Close.\r\n" + myWebsiteLink);
+		}
+
+	}
+}
